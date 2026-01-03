a89e62d40f98f562ff516e9d6bb7340ac751755a
Ivan Centamori <centamori@gmail.com>
2026-01-03 19:03:46 +0100

    minor ui fixes

--- public/template/terminal/page.php
+++ public/template/terminal/page.php
@@ -1,8 +1,11 @@
-<?php if (!defined('MICROBLO_APP')) { http_response_code(403); exit; } ?>
+<?php if (!defined('MICROBLO_APP')) {
+    http_response_code(403);
+    exit;
+} ?>
 <article class="page-single">
     <?= mb_post_content() ?>
 </article>
 
-<footer>
+<footer class="space-between">
     <p><a href="<?= mb_link('home') ?>">&larr; Back to Home</a></p>
 </footer>
--- public/template/terminal/single.php
+++ public/template/terminal/single.php
@@ -1,9 +1,12 @@
-<?php if (!defined('MICROBLO_APP')) { http_response_code(403); exit; } ?>
+<?php if (!defined('MICROBLO_APP')) {
+    http_response_code(403);
+    exit;
+} ?>
 <article class="post-single">
     <?= mb_post_content() ?>
 </article>
 
-<footer>
-    <p><a href="<?= mb_link('home') ?>">&larr; Back</a></p>
+<footer class="space-between">
+    <p><a href="<?= mb_link('home') ?>">&larr; Back to Home</a></p>
     <p><?= mb_post_date() ?></p>
 </footer>
